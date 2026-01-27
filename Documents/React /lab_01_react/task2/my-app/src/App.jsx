1ebec8b82af5a0967407de4cb65a72e53af7681b
Aliyanur <aliya.kaldibay01@gmail.com>
2026-01-28 00:39:52 +0500

    Counter task2 In Lab 1 lab_01 task2

---
+++ Documents/React /lab_01_react/task2/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "./components/Counter";
+
+function App(){
+  return(
+    <div>
+      <h1>My Minimal React App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
